954c23069a572ae7356ec33039db7da3e6b426b2
Thomas Naquiche <tommynaquiche@hotmail.com>
2023-04-30 00:03:40 +0200

    setting up deploy

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize,Op } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST,DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_DEPLOY } = process.env;
 const modelCharacter = require('./models/Character.js');
 const modelFavorite = require('./models/Favorite.js');
 /*
@@ -10,11 +10,17 @@ Recuerda pasarle la información de tu archivo '.env'.
 
 URL ----> postgres://DB_USER:DB_PASSWORD@DB_HOST/rickandmorty
 */
-const sequelize = new Sequelize(
-   // URL
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-   { logging: false, native: false }
-);
+// const sequelize = new Sequelize(
+//    // URL
+//    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//    { logging: false, native: false }
+// );
+
+const sequelize = new Sequelize(DB_DEPLOY, {
+   logging: false, // set to console.log to see the raw SQL queries
+   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+ });
+ 
 
 /*
 EJERCICIO 03
--- start.js
+++ start.js
@@ -1,22 +1,16 @@
-
+require('dotenv').config();
 const app = require ('./src/routes/server.js');
 const { db } = require("./src/DB_connection.js");
 const saveApiData = require ('./src/controllers/saveApiData.js');
 
-// app.listen(3001,() => {
-//   		console.log(`Server listening on port 3001`);
-// 	db.sync({ force: true }).then(()=>{
-// 			saveApiData()
-// 			console.log('db connected')
-// 	})
-//   });
+const PORT = process.env.PORT || 3001
 
-db.sync({ force: true }).then(async () => {
+db.sync({ force: false }).then(async () => {
     console.log('DB conectada, master');
     // console.log(await saveApiData());
-    await saveApiData();
+    //await saveApiData();
     app.listen(3001, () => {
-        console.log('Server on port 3001');
+        console.log(`Server on port ${PORT}`);
     })
 }).catch((error) => {
     console.log(error);
